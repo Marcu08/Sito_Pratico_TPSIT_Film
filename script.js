6006b62909311920043742c2e9ee51c776c4f3e0
Marcu <marcucci.francesco@itismeucci.com>
2026-05-21 11:48:15 +0200

    Aggiunta titolo e miglioramento css

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 let btnWatch = document.elementById("btn-watch");
 let video = document.elementById("video");  
+let btnInvia = document.elementById("btn-invia");
+
+btnWatch.addEventListener("click", function () {
+
+    const film = {
+        titolo: document.getElementById("titolo-film").value,
+        durata: document.getElementById("durata-film").value,
+        dataVisto: document.getElementById("data-visto").value,
+        vistoCinema: document.getElementById("visto-cinema").checked
+    }
+
+    localStorage.setItem("film", JSON.stringify(film));
+
+    alert("Dati del film salvati con successo!");
+});
