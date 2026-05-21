1ccfba3be1873b99a6329551ae703d1550634be1
Marcu <marcucci.francesco@itismeucci.com>
2026-05-21 12:37:33 +0200

    Inizio implementazione API gemini

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let btnWatch = document.elementById("btn-watch");
-let video = document.elementById("video");  
-let btnInvia = document.elementById("btn-invia");
+let btnInvia = document.getElementById("btn-invia");
 
-btnWatch.addEventListener("click", function () {
+
+const film = JSON.parse(localStorage.getItem("film"));
+
+if (film) {
+    console.log(film.titolo);
+    console.log(film.durata);
+    console.log(film.dataVisto);
+    console.log(film.vistoCinema ? "Visto al cinema" : "Non visto al cinema");
+}
+btnInvia.addEventListener("click", function () {
 
     const film = {
         titolo: document.getElementById("titolo-film").value,
         durata: document.getElementById("durata-film").value,
         dataVisto: document.getElementById("data-visto").value,
         vistoCinema: document.getElementById("visto-cinema").checked
-    }
+    };
 
     localStorage.setItem("film", JSON.stringify(film));
 
     alert("Dati del film salvati con successo!");
 });
+
+async function gestisciRichiesta() {
+    let risposta = await fetch("https://www.");
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+    });
+}   
