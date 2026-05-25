bdfc063362de3185f6773c8fc0f2443b09e72034
Marcu <marcucci.francesco@itismeucci.com>
2026-05-25 19:28:05 +0200

    Sistemato Funzione Local Storage script js

--- script.js
+++ script.js
@@ -110,28 +110,12 @@ if (document.getElementById('btnInvia')) {
 let btnInvia = document.getElementById("btn-invia");
 
 
-const film = JSON.parse(localStorage.getItem("film"));
+let filmSalvati =
+    JSON.parse(localStorage.getItem("listaFilm")) || [];
 
-if (film) {
-    console.log(film.titolo);
-    console.log(film.durata);
-    console.log(film.dataVisto);
-    console.log(film.vistoCinema ? "Visto al cinema" : "Non visto al cinema");
+if (filmSalvati.length > 0) {
+    listaFilm = filmSalvati;
 }
-btnInvia.addEventListener("click", function () {
-
-    const film = {
-        titolo: document.getElementById("titolo-film").value,
-        durata: document.getElementById("durata-film").value,
-        dataVisto: document.getElementById("data-visto").value,
-        vistoCinema: document.getElementById("visto-cinema").checked
-    };
-
-    localStorage.setItem("film", JSON.stringify(film));
-
-    alert("Dati del film salvati con successo!");
-});
-
 async function gestisciRichiesta() {
 
     const oggettoRichiesta = {
